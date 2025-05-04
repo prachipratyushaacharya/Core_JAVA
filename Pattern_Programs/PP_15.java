093a7139e464ae1cb8c5625fe27375587e42af7f
Prachi <prachipratyushaacharya@gmail.com>
2025-05-04 16:15:43 +0530

    update

---
+++ Pattern_Programs/PP_15.java
@@ -0,0 +1,12 @@
+public class PP_15 {
+    public static void main(Strin[] args)
+    {
+        for (int i= 1;i<=7;i++)
+        {
+            for (int j=1;j<=7;j++)
+            {
+                if ((i-j==2 && i<=5) || ())
+            }
+        }
+    }
+}
